13f916e2d699e3fe6e50071f65e9608df077c6f0
Thalles Torma <thallestorma@gmail.com>
2023-11-21 20:55:35 -0300

    Atualiza script_create_database.sql

--- server/script_create_database.sql
+++ server/script_create_database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE `dietsenior2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `dietsenior` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `dietsenior2`;
+USE `dietsenior`;
 
 
 CREATE TABLE `usuarios` (
